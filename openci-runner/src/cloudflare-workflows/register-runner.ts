ea6090d2ee55a287b4dec7c3ed275122cfbda44e
mafreud <freud427@gmail.com>
2025-12-16 12:39:08 +0900

    feat: add RegisterRunner workflow and update configuration

---
+++ openci-runner/src/cloudflare-workflows/register-runner.ts
@@ -0,0 +1,21 @@
+import {
+	WorkflowEntrypoint,
+	type WorkflowEvent,
+	type WorkflowStep,
+} from "cloudflare:workers";
+import type { WorkflowJobQueuedEvent } from "@octokit/webhooks-types";
+
+type Params = {
+	githubPayload: WorkflowJobQueuedEvent;
+	github_app_id: string;
+	github_app_private_key: string;
+	cloudflare_access_client_id: string;
+	cloudflare_access_client_secret: string;
+	incus_server_url: string;
+	openci_runner_base_image: string;
+	openci_runner_label: string;
+};
+
+export class RegisterRunner extends WorkflowEntrypoint<Env, Params> {
+	async run(_event: WorkflowEvent<Params>, _step: WorkflowStep) {}
+}
