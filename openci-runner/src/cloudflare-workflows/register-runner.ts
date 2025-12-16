dd738456f4caa5e6aef5b94298201059f5fe4cd4
mafreud <freud427@gmail.com>
2025-12-16 15:43:48 +0900

    chore: update vitest-pool-workers dependency to version 0.10.15
    feat: implement basic run method in RegisterRunner class to return true
    
    test: add unit test for RegisterRunner workflow to mock event and verify completion

--- openci-runner/src/cloudflare-workflows/register-runner.ts
+++ openci-runner/src/cloudflare-workflows/register-runner.ts
@@ -3,19 +3,9 @@ import {
 	type WorkflowEvent,
 	type WorkflowStep,
 } from "cloudflare:workers";
-import type { WorkflowJobQueuedEvent } from "@octokit/webhooks-types";
-
-type Params = {
-	github_payload: WorkflowJobQueuedEvent;
-	github_app_id: string;
-	github_app_private_key: string;
-	cloudflare_access_client_id: string;
-	cloudflare_access_client_secret: string;
-	incus_server_url: string;
-	openci_runner_base_image: string;
-	openci_runner_label: string;
-};
 
 export class RegisterRunner extends WorkflowEntrypoint<Env, Params> {
-	async run(_event: WorkflowEvent<Params>, _step: WorkflowStep) {}
+	async run(_event: WorkflowEvent<Params>, _step: WorkflowStep) {
+		return true;
+	}
 }
