af5a3075c831e5456ea9b46f120416a1c62bdd27
mafreud <freud427@gmail.com>
2025-12-30 17:32:34 +0000

    fix(lp): update Sanity project ID, enhance link component, and adjust blog post rendering

--- openci_runner/lp/src/components/link.tsx
+++ openci_runner/lp/src/components/link.tsx
@@ -6,14 +6,9 @@ export const Link = forwardRef(function Link(
   props: LinkProps & React.ComponentPropsWithoutRef<'a'>,
   ref: React.ForwardedRef<HTMLAnchorElement>,
 ) {
-  const { href, children, ...rest } = props
   return (
     <Headless.DataInteractive>
-      <NextLink href={href} legacyBehavior>
-        <a ref={ref} {...rest}>
-          {children}
-        </a>
-      </NextLink>
+      <NextLink ref={ref} {...props} />
     </Headless.DataInteractive>
   )
 })
