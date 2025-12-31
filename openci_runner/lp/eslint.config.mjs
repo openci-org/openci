3280d22118f684139103d44dcdc37fdaf29abe96
mafreud <freud427@gmail.com>
2025-12-31 18:14:30 +0000

    fix: standardize import quotes and format ESLint configuration for consistency

--- openci_runner/lp/eslint.config.mjs
+++ openci_runner/lp/eslint.config.mjs
@@ -1,13 +1,18 @@
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextTypescript from "eslint-config-next/typescript";
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals'
+import nextTypescript from 'eslint-config-next/typescript'
 
-const eslintConfig = [{
-  ignores: ['.next/**', 'node_modules/**', 'out/**', '.vercel/**'],
-}, ...nextCoreWebVitals, ...nextTypescript, {
-  rules: {
-    '@next/next/no-img-element': 'off',
-    'prefer-const': 'off',
+const eslintConfig = [
+  {
+    ignores: ['.next/**', 'node_modules/**', 'out/**', '.vercel/**'],
   },
-}]
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  {
+    rules: {
+      '@next/next/no-img-element': 'off',
+      'prefer-const': 'off',
+    },
+  },
+]
 
 export default eslintConfig
