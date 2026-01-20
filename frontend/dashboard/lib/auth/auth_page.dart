3ff694a195b18b45731a79c130dabffcc42a79fb
Masahiro Aoki <m.aoki@open-ci.io>
2026-01-20 05:57:55 +0000

    Add: Auth Page (#793)
    * feat: Implement Firebase authentication, refactor the application root and navigation, and integrate CocoaPods for dependency management.
    
    * Remove `formKey` variable definition.
    
    * feat: Implement authentication error display via snackbar and define new Freezed/JSON serializable models for workflow creation.
    
    * refactor: Remove `CreateWorkflowPage` and `ListPage` imports and the `pages` list from `main.dart`.
    
    * feat: Remove `firebase_ui_auth`, `app_links`, and `desktop_webview_auth` packages and their related dependencies.
    
    * refactor: Memoize TapGestureRecognizer in auth page.

---
+++ frontend/dashboard/lib/auth/auth_page.dart
@@ -0,0 +1,177 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class AuthPage extends HookWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final emailController = useTextEditingController();
+    final passwordController = useTextEditingController();
+    final isAgreed = useState(true);
+    final formKey = useMemoized(() => GlobalKey<FormState>());
+    final tapGestureRecognizer = useMemoized(() => TapGestureRecognizer());
+    final isLoading = useState(false);
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'OpenCI',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                SizedBox(height: 40),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(labelText: 'Email'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                  child: TextFormField(
+                    controller: passwordController,
+                    decoration: InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 8.0,
+                  children: [
+                    Checkbox(
+                      value: isAgreed.value,
+                      onChanged: (value) {
+                        isAgreed.value = value!;
+                      },
+                      visualDensity: VisualDensity.compact,
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    ),
+                    Text.rich(
+                      TextSpan(
+                        text: 'I agree to the ',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                        children: [
+                          TextSpan(
+                            text: 'Terms of Service',
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              decoration: TextDecoration.underline,
+                            ),
+                            recognizer: tapGestureRecognizer
+                              ..onTap = () {
+                                launchUrl(
+                                  Uri.parse(
+                                    'https://openci.org/terms-of-service',
+                                  ),
+                                );
+                              },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(fixedSize: Size(200, 20)),
+                  onPressed: (isAgreed.value && !isLoading.value)
+                      ? () async {
+                          if (formKey.currentState!.validate()) {
+                            isLoading.value = true;
+                            try {
+                              await FirebaseAuth.instance
+                                  .signInWithEmailAndPassword(
+                                    email: emailController.text,
+                                    password: passwordController.text,
+                                  );
+                            } catch (e) {
+                              if (!context.mounted) return;
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text('Error: $e'),
+                                  behavior: SnackBarBehavior.floating,
+                                ),
+                              );
+                            } finally {
+                              isLoading.value = false;
+                            }
+                          }
+                        }
+                      : null,
+                  child: Text('Log in'),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(200, 20),
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                  onPressed: (isAgreed.value && !isLoading.value)
+                      ? () async {
+                          if (formKey.currentState!.validate()) {
+                            isLoading.value = true;
+                            try {
+                              await FirebaseAuth.instance
+                                  .createUserWithEmailAndPassword(
+                                    email: emailController.text,
+                                    password: passwordController.text,
+                                  );
+                            } catch (e) {
+                              if (!context.mounted) return;
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text('Error: $e'),
+                                  behavior: SnackBarBehavior.floating,
+                                ),
+                              );
+                            } finally {
+                              isLoading.value = false;
+                            }
+                          }
+                        }
+                      : null,
+                  child: Text(
+                    'Create new account',
+                    style: TextStyle(
+                      color: Theme.of(context).secondaryHeaderColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          if (isLoading.value) ...[
+            const ModalBarrier(dismissible: false, color: Colors.black26),
+            const Center(child: CircularProgressIndicator.adaptive()),
+          ],
+        ],
+      ),
+    );
+  }
+}
---
+++ frontend/dashboard/lib/settings/settings_page.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () async {
+                  await FirebaseAuth.instance.signOut();
+                },
+                child: Text("Logout"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
