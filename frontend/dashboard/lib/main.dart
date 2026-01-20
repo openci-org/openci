88d6e5e6e865ba7b378ab29eb5b245edd1539257
mafreud <freud427@gmail.com>
2026-01-20 13:30:09 +0900

    refactor: Remove `CreateWorkflowPage` and `ListPage` imports and the `pages` list from `main.dart`.

--- frontend/dashboard/lib/main.dart
+++ frontend/dashboard/lib/main.dart
@@ -1,12 +1,9 @@
-import 'package:dashboard/create_workflow/create_workflow_page.dart';
 import 'package:dashboard/firebase_options.dart';
-import 'package:dashboard/list/list_page.dart';
 import 'package:dashboard/root.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final pages = [CreateWorkflowPage(), ListPage()];
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
