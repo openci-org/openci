2d5d7918af1880b6e9a53203e63dfc84158cdec5
Masahiro Aoki <m.aoki@open-ci.io>
2026-01-26 23:13:01 +0900

    Add: secret store (#898)

---
+++ frontend/dashboard/lib/secret_manager/secret_manager_provider.dart
@@ -0,0 +1,78 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:dashboard/firebase/firestore_paths.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'secret_manager_provider.freezed.dart';
+part 'secret_manager_provider.g.dart';
+
+@riverpod
+class SecretManager extends _$SecretManager {
+  @override
+  Stream build() {
+    return secretsStream();
+  }
+
+  Stream secretsStream() {
+    final userId = FirebaseAuth.instance.currentUser?.uid;
+    if (userId == null) {
+      return throw Exception('User not logged in');
+    }
+    return FirebaseFirestore.instance
+        .collection(secretsCollection)
+        .withConverter(
+          fromFirestore: (snapshot, _) => Secret.fromJson(snapshot.data()!),
+          toFirestore: (secret, _) => secret.toJson(),
+        )
+        .where('userId', isEqualTo: userId)
+        .snapshots()
+        .map((snapshot) => snapshot.docs.map((doc) => doc.data()).toList());
+  }
+}
+
+Future<void> addSecret(String name, String value) async {
+  try {
+    final functions = FirebaseFunctions.instanceFor(region: 'asia-northeast1');
+    await functions.httpsCallable(callableFunctionPath).call({
+      'name': name,
+      'value': value,
+    });
+  } catch (e) {
+    throw Exception('Failed to add secret: $e');
+  }
+}
+
+@freezed
+abstract class Secret with _$Secret {
+  const factory Secret({
+    required String id,
+    required String name,
+    required String userId,
+    String? pathToSecret,
+    @TimestampConverter() required DateTime createdAt,
+    @TimestampConverter() required DateTime updatedAt,
+  }) = _Secret;
+
+  factory Secret.fromJson(Map<String, Object?> json) => _$SecretFromJson(json);
+}
+
+class TimestampConverter implements JsonConverter<DateTime, dynamic> {
+  const TimestampConverter();
+
+  @override
+  DateTime fromJson(dynamic value) {
+    if (value is Timestamp) {
+      return value.toDate();
+    } else if (value is String) {
+      return DateTime.parse(value);
+    }
+    throw ArgumentError(
+      'Invalid type for DateTime conversion: ${value.runtimeType}',
+    );
+  }
+
+  @override
+  dynamic toJson(DateTime date) => Timestamp.fromDate(date);
+}
