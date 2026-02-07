feb59af751f3b3bd52192ab986182104927ffbab
Masahiro Aoki <m.aoki@open-ci.io>
2026-02-07 23:10:50 +0900

    feat: refactor firestore collection path .ts (#1065)
    * feat: refactor firestore collection path .ts
    
    * refactor: move `firestore-collection-paths.ts` into `src/` and update its import path.

---
+++ firebase/functions/src/firestore-collection-paths.ts
@@ -0,0 +1,8 @@
+export const buildJobsCollectionPath = "build_jobs_v0";
+export const secretsCollectionPath = "secrets_v0";
+export const workflowsCollectionPath = "workflows_v1";
+export const usersCollectionPath = "users_v0";
+export const orgsCollectionPath = "orgs_v0";
+
+export const runsSubcollectionPath = "runs";
+export const logsSubcollectionPath = "logs";
