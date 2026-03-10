a2afafaad41fec65bffe54cf025b4a9794745358
mafreud <freud427@gmail.com>
2026-03-10 15:57:15 +0900

    Bump to v0.6.8

--- apps/openci_worker_cli/bin/openci_worker_cli.dart
+++ apps/openci_worker_cli/bin/openci_worker_cli.dart
@@ -10,7 +10,7 @@ import 'package:googleapis_auth/auth_io.dart';
 import 'package:http/http.dart' as http;
 import 'package:process_run/process_run.dart';
 
-const String version = '0.6.7';
+const String version = '0.6.8';
 
 enum LogLevel { info, warning, error }
 
