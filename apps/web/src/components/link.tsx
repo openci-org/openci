647343ab697cf223d5f338cffc3f74904b2a3efe
mafreud <m.aoki@open-ci.io>
2026-04-17 14:58:08 +0900

    chore: remove web application directory and all associated assets

--- apps/web/src/components/link.tsx
+++
@@ -1,9 +0,0 @@
-import NextLink, { type LinkProps } from 'next/link'
-import { forwardRef } from 'react'
-
-export const Link = forwardRef(function Link(
-  props: LinkProps & React.ComponentPropsWithoutRef<'a'>,
-  ref: React.ForwardedRef<HTMLAnchorElement>,
-) {
-  return <NextLink ref={ref} {...props} />
-})
