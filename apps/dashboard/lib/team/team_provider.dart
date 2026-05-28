5dc203f2b229c4d9cb8d0ab1000576ebfc863025
mafreud <m.aoki@openci.org>
2026-05-28 14:56:46 +0900

    refactor: clean up unused imports and apply dartfmt to dashboard providers and pages

--- apps/dashboard/lib/team/team_provider.dart
+++ apps/dashboard/lib/team/team_provider.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dashboard/auth/auth_provider.dart';
 import 'package:dashboard/firebase/firestore.dart';
 import 'package:dashboard/users/user_provider.dart';
-import 'package:dashboard/utilities/date_time_converter.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:uuid/uuid.dart';
