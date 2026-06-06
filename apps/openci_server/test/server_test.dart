8113158c51080756d6440fbca2608919e698a5a9
mafreud <m.aoki@openci.org>
2026-06-06 18:29:35 +0900

    test: add server integration tests and update CI workflow to execute tests

--- apps/openci_server/test/server_test.dart
+++ apps/openci_server/test/server_test.dart
@@ -6,17 +6,31 @@ import 'package:test/test.dart';
 void main() {
   group('Server API Tests', () {
     late Handler handler;
+    const localHost = "http://localhost";
 
     setUp(() {
       handler = applyMiddleware(router);
     });
 
     test('GET / returns 200 and welcome message', () async {
-      final request = Request('GET', Uri.parse('http://localhost/'));
+      final request = Request('GET', Uri.parse('$localHost/'));
       final response = await handler(request);
 
       expect(response.statusCode, equals(200));
-      expect(await response.readAsString(), contains('OpenCI Server (Shelf) is running!'));
+      expect(
+        await response.readAsString(),
+        contains('OpenCI Server (Shelf) is running!'),
+      );
+    });
+
+    test('GET /invalid-path returns 404', () async {
+      final request = Request(
+        'GET',
+        Uri.parse('$localHost/invalid-path'),
+      );
+      final response = await handler(request);
+
+      expect(response.statusCode, equals(404));
     });
   });
 }
