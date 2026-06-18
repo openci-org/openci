c8c5c331a47dd38aa94d9cdc15c63edac02e28d0
mafreud <m.aoki@openci.org>
2026-06-18 14:55:07 +0900

    refactor: update root status message and explicitly mount health check route in server router

--- apps/openci_server/routes/index.dart
+++ apps/openci_server/routes/index.dart
@@ -1,5 +1,5 @@
 import 'package:dart_frog/dart_frog.dart';
 
 Response onRequest(RequestContext context) {
-  return Response(body: 'OpenCI Server (Shelf) is running!');
+  return Response(body: 'OpenCI Server is running!');
 }
