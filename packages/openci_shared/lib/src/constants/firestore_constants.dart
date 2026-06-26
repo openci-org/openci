a7e9103d1e20703348140d5387acbd177f43751d
Masahiro Aoki <m.aoki@openci.org>
2026-06-26 15:07:18 +0900

    refactor: remove unused CI/CD fix request functionality and associated Firestore rules (#2366)

--- packages/openci_shared/lib/src/constants/firestore_constants.dart
+++ packages/openci_shared/lib/src/constants/firestore_constants.dart
@@ -1,7 +1,6 @@
 const teamsCollection = 'teams_v0';
 const usersCollection = 'users_v0';
 const buildJobsCollection = 'build_jobs_v0';
-const ciCdFixRequestsCollection = 'ci_cd_fix_requests_v0';
 const workerInstancesCollection = 'worker_instances_v0';
 const workflowsCollection = 'workflows_v1';
 const workflowFilesCollection = 'workflow_files_v0';
