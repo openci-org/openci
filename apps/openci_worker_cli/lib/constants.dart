4a8b0cbf9aa10afae6c2a2315b95541130957aca
mafreud <m.aoki@openci.org>
2026-07-02 22:54:38 +0900

    refactor: remove redundant comments from constants file

--- apps/openci_worker_cli/lib/constants.dart
+++ apps/openci_worker_cli/lib/constants.dart
@@ -2,15 +2,12 @@ import 'package:openci_worker_cli/src/version.dart';
 
 const version = packageVersion;
 
-// macOS (Lume VM)
 const baseVmName = 'tahoe-base_v1.1.1';
 const sshUser = 'admin';
 const sshPassword = 'admin';
 
-// Linux (Docker)
 const dockerImage = 'openci-ubuntu:latest';
 
-// Exit codes for supervisor communication
 const exitCodeUpdateRequested = 42;
 
 const firebaseSignInUrl =
