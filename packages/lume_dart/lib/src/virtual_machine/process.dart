f9196ea9bf9036182b838a7359bf4c6db4912dde
Masahiro Aoki <m.aoki@openci.org>
2026-07-05 23:53:24 +0900

    feat: add stop VM functionality and refactor process execution to be injectable (#2432)

---
+++ packages/lume_dart/lib/src/virtual_machine/process.dart
@@ -0,0 +1,6 @@
+import 'dart:io';
+
+typedef ProcessRunner = Future<ProcessResult> Function(
+  String executable,
+  List<String> arguments,
+);
---
+++ packages/lume_dart/lib/src/virtual_machine/stop.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+import 'process.dart';
+
+Future<void> stop({
+  required String name,
+  bool showLogs = true,
+  ProcessRunner runProcess = Process.run,
+}) async {
+  if (showLogs) {
+    print('Stopping macOS VM "$name" via Lume...');
+  }
+
+  final result = await runProcess('lume', ['stop', name]);
+  if (result.exitCode != 0) {
+    throw StateError('Failed to stop VM via Lume: ${result.stderr}');
+  }
+  if (showLogs) {
+    print('VM stopped successfully: "$name"');
+  }
+}
