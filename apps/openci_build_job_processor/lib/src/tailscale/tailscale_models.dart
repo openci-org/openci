a07c9f800a59979377f7d02e38712ae1f8d5f1d8
Masahiro Aoki <m.aoki@openci.org>
2026-07-13 14:54:56 +0900

    feat: implement TailscaleJsonConverter and update service to use explicit converter (#2461)

--- apps/openci_build_job_processor/lib/src/tailscale/tailscale_models.dart
+++ apps/openci_build_job_processor/lib/src/tailscale/tailscale_models.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-import 'package:chopper/chopper.dart';
 import 'package:freezed_annotation/freezed_annotation.dart' hide JsonConverter;
 
 part 'tailscale_models.freezed.dart';
@@ -25,30 +23,4 @@ abstract class TailscaleDevicesResponse with _$TailscaleDevicesResponse {
 
   factory TailscaleDevicesResponse.fromJson(Map<String, dynamic> json) =>
       _$TailscaleDevicesResponseFromJson(json);
-}
-
-class TailscaleJsonConverter extends JsonConverter {
-  const TailscaleJsonConverter();
-
-  @override
-  FutureOr<Response<BodyType>> convertResponse<BodyType, InnerType>(
-    Response response,
-  ) async {
-    final jsonResponse = await super.convertResponse<dynamic, dynamic>(
-      response,
-    );
-    final body = jsonResponse.body;
-    final convertedBody = _convertToType<InnerType>(body);
-    return jsonResponse.copyWith<BodyType>(body: convertedBody as BodyType);
-  }
-
-  dynamic _convertToType<T>(dynamic json) {
-    if (json == null) return null;
-    if (T == TailscaleDevicesResponse) {
-      return TailscaleDevicesResponse.fromJson(
-        Map<String, dynamic>.from(json as Map),
-      );
-    }
-    return json;
-  }
 }
