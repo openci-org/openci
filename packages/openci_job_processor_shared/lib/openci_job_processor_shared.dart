430c8dbede45abd941000a036552cc2e95b1ef11
mafreud <m.aoki@openci.org>
2026-07-22 12:34:06 +0900

    feat: add openci_job_processor_shared package with VmService interface

---
+++ packages/openci_job_processor_shared/lib/openci_job_processor_shared.dart
@@ -0,0 +1,3 @@
+library;
+
+export 'src/vm/vm_service.dart';
---
+++ packages/openci_job_processor_shared/lib/src/vm/vm_service.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+abstract class VmService {
+  Future<void> prepare({
+    required String baseInstanceName,
+    required String containerName,
+    required void Function() onCreated,
+  });
+
+  Future<void> cleanup(String containerName);
+
+  Future<int> executeCommandStreaming({
+    required String containerName,
+    required List<String> command,
+    required void Function(String line) onLog,
+    required Future<bool> Function() isCancelled,
+  });
+
+  Future<void> writeFile(
+    String containerName,
+    String filePath,
+    String content, {
+    String? mode,
+  });
+}
