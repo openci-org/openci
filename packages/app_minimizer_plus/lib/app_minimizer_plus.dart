30c875c5c6b4b5560a1edc6a4531868627f37878
mafreud <m.aoki@openci.org>
2026-08-01 09:57:08 +0900

    refactor: remove app_minimizer_plus package and its example project

--- packages/app_minimizer_plus/lib/app_minimizer_plus.dart
+++
@@ -1,9 +0,0 @@
-import 'app_minimizer_api.g.dart';
-
-class AppMinimizer {
-  static final _api = AppMinimizerHostApi();
-
-  static Future<void> minimize() async {
-    await _api.minimize();
-  }
-}
