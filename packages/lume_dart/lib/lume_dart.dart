148d5ee7136ae108b19136d50c9bca6541080396
mafreud <m.aoki@openci.org>
2026-08-01 20:44:36 +0900

    refactor: remove lume_dart package and its references from the workspace

--- packages/lume_dart/lib/lume_dart.dart
+++
@@ -1,6 +0,0 @@
-export 'src/virtual_machine/clone.dart';
-export 'src/virtual_machine/delete.dart';
-export 'src/virtual_machine/ls.dart';
-export 'src/virtual_machine/models.dart';
-export 'src/virtual_machine/run.dart';
-export 'src/virtual_machine/stop.dart';
