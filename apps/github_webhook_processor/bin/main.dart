e98af3a500284fa1e32fa8714c09f46dd295e922
mafreud <m.aoki@openci.org>
2026-08-17 13:58:04 +0900

    refactor: replace github_webhook_processor library with a minimal entry point for Sentry-monitored execution

---
+++ apps/github_webhook_processor/bin/main.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:sentry/sentry.dart';
+
+Future<void> main() async => genuineCiRunZonedGuarded(() async {
+  () async {};
+});
+
+Future<void> genuineCiRunZonedGuarded(Future<void> Function() body) async =>
+    runZonedGuarded(body, (error, stackTrace) async {
+      stderr.writeln('FATAL UNCAUGHT ERROR: $error');
+      stderr.writeln(stackTrace);
+      await Sentry.captureException(error, stackTrace: stackTrace);
+      exit(1);
+    });
--- apps/github_webhook_processor/lib/github_webhook_processor.dart
+++
@@ -1,3 +0,0 @@
-export 'src/github_auth/generate_github_app_jwt.dart';
-export 'src/github_auth/github_auth_client.dart';
-export 'src/processor_config.dart';
