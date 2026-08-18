ef585e7a01c83e1df394b56b384b9dc3c87c38a6
mafreud <m.aoki@openci.org>
2026-08-18 16:42:08 +0900

    refactor: replace runZonedGuarded with genuineCiRunZonedGuarded and remove unused dart:io import

--- apps/build_job_executor/bin/main.dart
+++ apps/build_job_executor/bin/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:build_job_executor/build_job_executor.dart';
 import 'package:logging/logging.dart';
@@ -8,45 +7,36 @@ import 'package:sentry/sentry.dart';
 
 final _log = Logger('JobProcessorMain');
 
-Future<void> main() async {
-  await runZonedGuarded(
-    () async {
-      final config = ProcessorConfig.fromEnvironment();
+Future<void> main() async => genuineCiRunZonedGuarded(() async {
+  () async {
+    final config = ProcessorConfig.fromEnvironment();
 
-      await initializeSentry(config.sentryDsn);
+    await initializeSentry(config.sentryDsn);
 
-      final jobPoller = JobPoller(config: config);
-      final apiService = createOpenCiChopperClient(
-        baseUrl: config.serverUrl,
-        tokenProvider: () => config.internalApiKey,
-        services: [OpenCiApiService.create()],
-      ).getService<OpenCiApiService>();
+    final jobPoller = JobPoller(config: config);
+    final apiService = createOpenCiChopperClient(
+      baseUrl: config.serverUrl,
+      tokenProvider: () => config.internalApiKey,
+      services: [OpenCiApiService.create()],
+    ).getService<OpenCiApiService>();
 
-      final executor = BuildJobExecutor.create(
-        apiService: apiService,
-        config: config,
-      );
+    final executor = BuildJobExecutor.create(
+      apiService: apiService,
+      config: config,
+    );
 
-      _log.info(
-        'Orchard Config: name=${config.orchardServiceAccountName}, tokenLength=${config.orchardServiceAccountToken.length}, url=${config.orchardApiUrl}',
-      );
-      _log.info('Starting Job Processor listening on Stream for queued jobs');
+    _log.info(
+      'Orchard Config: name=${config.orchardServiceAccountName}, tokenLength=${config.orchardServiceAccountToken.length}, url=${config.orchardApiUrl}',
+    );
+    _log.info('Starting Job Processor listening on Stream for queued jobs');
 
-      await for (final job in jobPoller.watchClaimedJobs()) {
-        try {
-          await executor.execute(job);
-        } catch (e, s) {
-          _log.severe('Error executing job ${job.id}: $e', e, s);
-          unawaited(Sentry.captureException(e, stackTrace: s));
-        }
+    await for (final job in jobPoller.watchClaimedJobs()) {
+      try {
+        await executor.execute(job);
+      } catch (e, s) {
+        _log.severe('Error executing job ${job.id}: $e', e, s);
+        unawaited(Sentry.captureException(e, stackTrace: s));
       }
-    },
-
-    (error, stackTrace) async {
-      stderr.writeln('FATAL UNCAUGHT ERROR: $error');
-      stderr.writeln(stackTrace);
-      await Sentry.captureException(error, stackTrace: stackTrace);
-      exit(1);
-    },
-  );
-}
+    }
+  };
+});
