80595a82ca19b3c87e23fc36d695235783513472
mafreud <m.aoki@openci.org>
2026-08-21 01:20:26 +0900

    feat: initialize genuine_ci_cli project with scaffolding, dependencies, and base command structure

---
+++ apps/genuine_ci_cli/lib/genuine_ci_cli.dart
@@ -0,0 +1,10 @@
+/// genuine_ci_cli, GenuineCI CLI
+///
+/// ```sh
+/// # activate genuine_ci_cli
+/// dart pub global activate genuine_ci_cli
+///
+/// # see usage
+/// genuineci --help
+/// ```
+library;
