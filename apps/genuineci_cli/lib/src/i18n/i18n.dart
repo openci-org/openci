89ea59b712b9181aaecd5d150990b2946c7f068a
Masahiro Aoki <m.aoki@openci.org>
2026-08-30 00:35:14 +0900

    Refactor read (#2543)
    * feat(cli_config): add @visibleForTesting annotation to customFilePath parameter
    
    * refactor(i18n): replace CliConfig with EditLanguageConfig for language management
    
    * refactor(cli_config, i18n): unify read/write methods and improve error handling

--- apps/genuineci_cli/lib/src/i18n/i18n.dart
+++ apps/genuineci_cli/lib/src/i18n/i18n.dart
@@ -3,8 +3,6 @@ import '../gen/strings.g.dart';
 
 export '../gen/strings.g.dart';
 
-/// Initializes i18n / slang translations.
-/// Defaults to English, unless Japanese ('ja') is configured.
 void initI18n({EditLanguageConfig? languageConfig}) {
   final langConfig = languageConfig ?? EditLanguageConfig();
   final configuredLang = langConfig.getLanguage();
@@ -22,14 +20,14 @@ class EditLanguageConfig {
   EditLanguageConfig({CliConfig? config}) : _config = config ?? CliConfig();
 
   String? getLanguage() {
-    final data = _config.readSync();
+    final data = _config.read();
     return data['language'] as String?;
   }
 
   void setLanguage(String languageCode) {
-    final data = _config.readSync();
+    final data = _config.read();
     data['language'] = languageCode;
-    _config.writeSync(data);
+    _config.write(data);
 
     final locale = AppLocaleUtils.parse(languageCode);
     LocaleSettings.setLocaleSync(locale);
