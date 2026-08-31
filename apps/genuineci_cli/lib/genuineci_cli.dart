5f4e0a94d74f767a0d55b90ab7207b4acfa802af
mafreud <m.aoki@openci.org>
2026-08-31 17:52:14 +0900

    refactor(genuineci_cli): update exports in genuineci_cli.dart and clean up imports in credential_store_test.dart

--- apps/genuineci_cli/lib/genuineci_cli.dart
+++ apps/genuineci_cli/lib/genuineci_cli.dart
@@ -5,6 +5,7 @@ export 'src/commands/login_command.dart';
 export 'src/commands/use_command.dart';
 export 'src/config/cli_config.dart';
 export 'src/config/cli_config_data.dart';
+export 'src/credential_store/credential_config.dart';
 export 'src/credential_store/credential_store.dart';
 export 'src/i18n/i18n.dart';
 export 'src/json_file_store/json_file_store.dart';
