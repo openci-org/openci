8ab1447fe92b78368bcf4494525d7e6f5cdfe24d
Masahiro Aoki <m.aoki@openci.org>
2026-09-13 19:37:35 +0900

    refactor: remove unused step log POST endpoint (#2706)

--- packages/openci_shared/lib/src/api/openci_api_service.dart
+++ packages/openci_shared/lib/src/api/openci_api_service.dart
@@ -204,17 +204,6 @@ abstract class OpenCiApiService extends ChopperService {
     @Path('stepId') String stepId,
   );
 
-  @POST(
-    path: '/builds/{id}/runs/{runId}/steps/{stepId}/logs',
-    timeout: _timeout,
-  )
-  Future<Response<void>> appendStepLog(
-    @Path('id') String buildJobId,
-    @Path('runId') String runId,
-    @Path('stepId') String stepId,
-    @Body() Map<String, dynamic> body,
-  );
-
   @GET(path: '/builds/commits', timeout: _timeout)
   Future<Response<List<CicdCommitGroup>>> getCommitGroups(
     @Query('teamId') String teamId,
